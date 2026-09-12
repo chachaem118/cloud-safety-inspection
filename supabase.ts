e4a6de9cd8c0cfe5f8795d4aa22778ff3568ffa0
chachaem118 <chachaem118@gmail.com>
2026-09-13 06:13:34 +0700

    Delete supabase.ts

--- supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://scniicwahrgbahtpakkf.supabase.co';
-const supabaseAnonKey = 'sb_publishable_HqRPGJ3p2k7bE91bOHN-hQ_b0BoLp8o';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
